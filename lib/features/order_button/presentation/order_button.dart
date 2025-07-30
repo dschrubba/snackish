484a0b5f6c9c166884621701764d2383860ea52f
Daniel Schrubba <daniel.schrubba.91@gmail.com>
2025-07-30 10:41:19 +0200

    Glow for order Button

--- lib/features/order_button/presentation/order_button.dart
+++ lib/features/order_button/presentation/order_button.dart
@@ -20,9 +20,16 @@ class OrderButton extends StatelessWidget {
     blurRadius: 15,
   );
 
+  static const BoxShadow glow = BoxShadow(
+    offset: Offset(0, 30),
+    blurStyle: BlurStyle.normal,
+    color: SnackishColors.orderNowBtnGlow,
+    blurRadius: 90,
+  );
+
   static const Gradient candyGradient = RadialGradient(
     radius: 4,
-    center: AlignmentGeometry.xy(1,3),
+    center: AlignmentGeometry.xy(1, 3),
     colors: [
       SnackishColors.orderNowBtnGradientA,
       SnackishColors.orderNowBtnGradientB,
@@ -65,39 +72,42 @@ class OrderButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return ConstrainedBox(
       constraints: BoxConstraints(maxHeight: 56, maxWidth: maxWidth),
-      child: ClipRRect(
-        clipBehavior: Clip.antiAlias,
-        borderRadius: BorderRadiusGeometry.circular(borderRadius),
-        child: Stack(
-          children: [
-            // Background with candy gradient, needed as base color behind border
-            Container(
-              decoration: BoxDecoration(
-                gradient: candyGradient
-              ),
-            ),
-            // Button Border
-            Container(
-              decoration: BoxDecoration(
-                gradient: borderGradient
-              ),
-            ),
-            // Padding to simulate border
-            Padding(
-              padding: EdgeInsetsGeometry.all(borderWidth),
-              child: ClipRRect(
-                borderRadius: BorderRadiusGeometry.circular(borderRadius - (borderWidth / 2)),
-                child: Container(
-                  decoration: BoxDecoration(
-                  gradient: candyGradient
+      child: Stack(
+        children: [
+          ClipRRect(
+            clipBehavior: Clip.antiAlias,
+            borderRadius: BorderRadiusGeometry.circular(borderRadius),
+            child: Stack(
+              children: [
+                // Background with candy gradient, needed as base color behind border
+                Container(decoration: BoxDecoration(gradient: candyGradient)),
+                // Button Border
+                Container(decoration: BoxDecoration(gradient: borderGradient)),
+                // Padding to simulate border
+                Padding(
+                  padding: EdgeInsetsGeometry.all(borderWidth),
+                  child: ClipRRect(
+                    borderRadius: BorderRadiusGeometry.circular(
+                      borderRadius - (borderWidth / 2),
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(gradient: candyGradient),
+                    ),
+                  ),
                 ),
-                            ),
-              ),
+              ],
             ),
-            // Actual button for interaction
-            textButton(buttonText)
-          ],
-        ),
+          ),
+          // Glow
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadiusGeometry.circular(borderRadius),
+              boxShadow: [glow],
+            ),
+          ),
+          // Actual button for interaction
+          textButton(buttonText),
+        ],
       ),
     );
   }
