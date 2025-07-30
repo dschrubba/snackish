4c1f824c4d4b0a4d0a3f8822275d78cdbcc6d5fd
Daniel Schrubba <daniel.schrubba.91@gmail.com>
2025-07-30 10:27:57 +0200

    Added URLs for easy image access

---
+++ lib/common/static_images.dart
@@ -0,0 +1,4 @@
+class StaticImages {
+  static const String backgroundMainScreen  = "assets/backgrounds/bg_mainscreen.png";
+  static const String backgroundStartScreen = "assets/backgrounds/bg_startscreen.png";
+}
