90e1777770db1c875a3ee3652974b333abeb636e
Daniel Schrubba <daniel.schrubba.91@gmail.com>
2025-07-30 10:27:39 +0200

    Fixed border for glass card

--- lib/common/glass_card.dart
+++ lib/common/glass_card.dart
@@ -11,23 +11,25 @@ class GlassCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
+      clipBehavior: Clip.antiAlias,
       borderRadius: BorderRadiusGeometry.circular(borderRadius),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Color(0x01FFFFFF),  
-          border: Border.all(
-            strokeAlign: BorderSide.strokeAlignInside,
-            color: Colors.white, 
-            width: borderWidth)
+      child: BackdropFilter(
+        blendMode: BlendMode.srcATop,
+        filter: ImageFilter.blur(sigmaX: 32, sigmaY: 32),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Color(0x01FFFFFF),
+            borderRadius: BorderRadiusGeometry.circular(borderRadius),
+            border: Border.all(
+              strokeAlign: BorderSide.strokeAlignInside,
+              color: Colors.white.withAlpha((255/100*30).round()),
+              width: borderWidth,
+            ),
+          ),
+          child: Padding(
+            padding: EdgeInsetsGeometry.all(32), child: child),
         ),
-        child:  BackdropFilter(
-        filter: ImageFilter.blur(
-          sigmaX: 32,
-          sigmaY: 32
-        ),
-        child: Padding(padding: EdgeInsetsGeometry.all(32),child:  child),
       ),
-      )
     );
   }
 }
